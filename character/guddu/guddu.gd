47d542caf6d14ef088bd19fe2149bf08a5d44b66
James S. Wang <jjameswwang@gmail.com>
2024-06-17 21:46:21 -0700

    Player movement

--- character/guddu/guddu.gd
+++ character/guddu/guddu.gd
@@ -1,4 +1,6 @@
-extends Node2D
+extends CharacterBody2D
+
+@export var max_speed = 50
 
 
 # Called when the node enters the scene tree for the first time.
@@ -7,5 +9,16 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _process(_delta):
+	var input_vector = Vector2.ZERO
+	if Input.is_action_pressed("move_left"):
+		input_vector += Vector2(-1, 0)
+	if Input.is_action_pressed("move_right"):
+		input_vector += Vector2(1, 0)
+	if Input.is_action_pressed("move_up"):
+		input_vector += Vector2(0, -1)
+	if Input.is_action_pressed("move_down"):
+		input_vector += Vector2(0, 1)
+
+	velocity = input_vector.normalized() * max_speed
+	move_and_slide()
